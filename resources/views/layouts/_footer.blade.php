23c0c6ce2cd76982b450577bff462eba2b3c75dc
long.wang <long.wang@qnbar.com>
2019-03-30 07:58:05 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://weibo.com/5651721966/profile?topnav=1&wvr=6" target="_blank">Fendouluo</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:422902732@qq.com">联系我们</a></p>
+  </div>
+</footer>
