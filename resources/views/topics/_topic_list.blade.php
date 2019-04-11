ce465c74dcb2d53d6c876b48099b7e0203ec4f64
long.wang <long.wang@qnbar.com>
2019-04-11 06:50:19 +0000

    解决无法打开话题界面的bug

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -1,36 +1,50 @@
-<ul class="list-unstyled">
-  @foreach ($replies as $index => $reply)
-    <li class=" media" name="reply{{ $reply->id }}" id="reply{{ $reply->id }}">
-      <div class="media-left">
-        <a href="{{ route('users.show', [$reply->user_id]) }}">
-          <img class="media-object img-thumbnail mr-3" alt="{{ $reply->user->name }}" src="{{ $reply->user->avatar }}" style="width:48px;height:48px;" />
-        </a>
-      </div>
-
-      <div class="media-body">
-        <div class="media-heading mt-0 mb-1 text-secondary">
-          <a href="{{ route('users.show', [$reply->user_id]) }}" title="{{ $reply->user->name }}">
-            {{ $reply->user->name }}
+@if (count($topics))
+  <ul class="list-unstyled">
+    @foreach ($topics as $topic)
+      <li class="media">
+        <div class="media-left">
+          <a href="{{ route('users.show', [$topic->user_id]) }}">
+            <img class="media-object img-thumbnail mr-3" style="width: 52px; height: 52px;" src="{{ $topic->user->avatar }}" title="{{ $topic->user->name }}">
           </a>
-          <span class="text-secondary"> • </span>
-          <span class="meta text-secondary" title="{{ $reply->created_at }}">{{ $reply->created_at->diffForHumans() }}</span>
+        </div>
 
-          {{-- 回复删除按钮 --}}
-          <span class="meta float-right ">
-            <a title="删除回复">
-              <i class="far fa-trash-alt"></i>
+        <div class="media-body">
+
+          <div class="media-heading mt-0 mb-1">
+            <a href="{{ $topic->link() }}" title="{{ $topic->title }}">
+              {{ $topic->title }}
             </a>
-          </span>
-        </div>
-        <div class="reply-content text-secondary">
-          {!! $reply->content !!}
-        </div>
-      </div>
-    </li>
+            <a class="float-right" href="{{ route('topics.show', [$topic->id]) }}">
+              <span class="badge badge-secondary badge-pill"> {{ $topic->reply_count }} </span>
+            </a>
+          </div>
 
-    @if ( ! $loop->last)
-      <hr>
-    @endif
+          <small class="media-body meta text-secondary">
 
-  @endforeach
-</ul>
+            <a class="text-secondary" href="{{ route('categories.show', $topic->category_id) }}" title="{{ $topic->category->name }}">
+              <i class="far fa-folder"></i>
+              {{ $topic->category->name }}
+            </a>
+            <span> • </span>
+            <a class="text-secondary" href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user->name }}">
+              <i class="far fa-user"></i>
+              {{ $topic->user->name }}
+            </a>
+            <span> • </span>
+            <i class="far fa-clock"></i>
+            <span class="timeago" title="最后活跃于：{{ $topic->updated_at }}">{{ $topic->updated_at->diffForHumans() }}</span>
+          </small>
+
+        </div>
+      </li>
+
+      @if ( ! $loop->last)
+        <hr>
+      @endif
+
+    @endforeach
+  </ul>
+
+@else
+  <div class="empty-block">暂无数据 ~_~ </div>
+@endif
